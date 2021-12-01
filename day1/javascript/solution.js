f46b040afc7ff9fb08e352a99fa61b78e4b6cda6
Pancake <71993095+Cinnamonsroll@users.noreply.github.com>
2021-12-01 10:31:19 -0500

    Create solution.js

---
+++ day1/javascript/solution.js
@@ -0,0 +1,7 @@
+const { readFileSync } = require("node:fs");
+const input = readFileSync("./input.txt", "utf-8").split("\n");
+let increased = 1, length = input.length;
+for (let i = 1; i < length; i++) {
+  if (input[i] > input[i - 1]) increased++;
+}
+console.log(increased);
