080cd8cff3a36c6680aac7a54b27af1d1be1f204
Pancake <71993095+Cinnamonsroll@users.noreply.github.com>
2021-12-01 10:32:25 -0500

    Delete solution.js

--- day1/solution.js
+++
@@ -1,8 +0,0 @@
-const { readFileSync } = require("node:fs");
-const input = readFileSync("./input.txt", "utf-8").split("\n");
-const { performance } = require("perf_hooks");
-let increased = 1, length = input.length;
-for (let i = 1; i < length; i++) {
-  if (input[i] > input[i - 1]) increased++;
-}
-console.log(increased);
