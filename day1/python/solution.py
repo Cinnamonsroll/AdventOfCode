d04e1a7d953a0f50b290e762ed8b273b147e6a31
Pancake <71993095+Cinnamonsroll@users.noreply.github.com>
2021-12-01 15:07:27 -0500

    Update solution.py

--- day1/python/solution.py
+++ day1/python/solution.py
@@ -1,1 +1,10 @@
-
+with open('./input.txt', 'r') as handle:
+    text = handle.read().splitlines()
+    i = 0
+    length = len(text)
+    increased = 1
+    while i < length:
+        if text[i] > text[i-1]:
+            increased += 1
+        i += 1
+    print(increased)
