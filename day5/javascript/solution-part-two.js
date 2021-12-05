8b2846fca5fb9fc99d3cb6bf5592acd00ce168e5
Pancake <71993095+Cinnamonsroll@users.noreply.github.com>
2021-12-05 01:18:51 -0500

    Update solution-part-two.js

--- day5/javascript/solution-part-two.js
+++ day5/javascript/solution-part-two.js
@@ -2,13 +2,11 @@ let { readFileSync } = require("node:fs");
 let { EOL } = require("node:os");
 const spots = new Map(), parsedInput = readFileSync("../input.txt", "utf-8").split("\n").map((line) => line.replace(" -> ", ",").split(",").map(x => +x))
 for (const [x1, y1, x2, y2] of parsedInput) {
-  if (x1 === x2 || y1 === y2 || true) {
     const dx = Math.sign(x2 - x1);
     const dy = Math.sign(y2 - y1);
     for (let x = x1, y = y1; x !== x2 + dx || y !== y2 + dy; x += dx, y += dy) {
       const key = [x, y].join`,`
       spots.set(key, (spots.get(key) ?? 0) + 1);
-    }
   }
 }
 console.log([...spots.values()].filter((val) => val > 1).length)
