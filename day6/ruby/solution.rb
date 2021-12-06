9ecd3fc6919068754a298c42b0e6909cfbe6e012
Pancake <71993095+Cinnamonsroll@users.noreply.github.com>
2021-12-06 08:29:23 -0500

    Create solution.rb

---
+++ day6/ruby/solution.rb
@@ -0,0 +1,10 @@
+input = File.read('./input.txt').scan(/\d+/).map(&:to_i)
+[80].each do |days|
+    count = Array.new(9,0)
+    input.each {|i| count[i] += 1 }
+    days.times do
+        count[7] += count[0]
+        count = count.rotate(1)
+    end
+    p count.sum
+end
