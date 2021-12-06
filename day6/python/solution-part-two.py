d6949541a51f15ee304f40de8406dff509405b1f
Pancake <71993095+Cinnamonsroll@users.noreply.github.com>
2021-12-06 08:02:18 -0500

    Create solution-part-two.py

---
+++ day6/python/solution-part-two.py
@@ -0,0 +1,7 @@
+with open('../input.txt', 'r') as handle:
+    ageCounts = [0] * 9
+    for age in [int(i) for i in handle.read().split(",")]:
+        ageCounts[age] += 1
+    for i in range(256):
+        ageCounts[(i + 7) % 9] += ageCounts[i % 9]
+    print(sum(ageCounts))
