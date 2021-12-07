83a37bd81b09e60ec0b658a810fae5e453845752
Pancake <71993095+Cinnamonsroll@users.noreply.github.com>
2021-12-07 07:49:10 -0500

    Update solution.js

--- day7/javascript/solution.js
+++ day7/javascript/solution.js
@@ -1,2 +1,2 @@
-const { readFileSync } = require("fs"), { EOL } = require("os"), positions = readFileSync("./input.txt", "utf-8").split(",").map(Number).sort((a, b) => a - b), median = positions[positions.length/2];
+const { readFileSync } = require("fs"), { EOL } = require("os"), positions = readFileSync("../input.txt", "utf-8").split(",").map(Number).sort((a, b) => a - b), median = positions[positions.length/2];
 console.log(positions.reduce((count, cur) => count + Math.abs(median - cur), 0))
