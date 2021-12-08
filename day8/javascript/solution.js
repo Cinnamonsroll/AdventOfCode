4eddcef09f8d3e267d02867f09205de208f9bf99
Pancake <71993095+Cinnamonsroll@users.noreply.github.com>
2021-12-08 07:49:25 -0500

    Update solution.js

--- day8/javascript/solution.js
+++ day8/javascript/solution.js
@@ -1,4 +1,4 @@
-let { readFileSync } = require("fs"), { EOL } = require("os"), lines = readFileSync("./input.txt", "utf-8").split(/\r?\n/), segements = [2,3,4,7], uniqueValues = 0;
+let { readFileSync } = require("fs"), { EOL } = require("os"), lines = readFileSync("../input.txt", "utf-8").split(/\r?\n/), segements = [2,3,4,7], uniqueValues = 0;
 for(let i = 0; i < lines.length; i++){
     lines[i] =  lines[i].split(" | ")[1].split(" ")
     for(let j = 0; j < lines[i].length;  j++){
