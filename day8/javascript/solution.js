3a95a8b2ca325e55532ce9243df795ae71df74c8
Pancake <71993095+Cinnamonsroll@users.noreply.github.com>
2021-12-08 07:48:43 -0500

    Create solution.js

---
+++ day8/javascript/solution.js
@@ -0,0 +1,10 @@
+let { readFileSync } = require("fs"), { EOL } = require("os"), lines = readFileSync("./input.txt", "utf-8").split(/\r?\n/), segements = [2,3,4,7], uniqueValues = 0;
+for(let i = 0; i < lines.length; i++){
+    lines[i] =  lines[i].split(" | ")[1].split(" ")
+    for(let j = 0; j < lines[i].length;  j++){
+        if(segements.includes(lines[i][j].length)){
+            uniqueValues++
+        }
+    }
+}
+console.log(uniqueValues)
